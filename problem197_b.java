2f02017a7072e78d3b646b086e6e27424baf51a2
Jonathan Lu <56990146+jnl27@users.noreply.github.com>
2019-12-30 17:56:25 -0500

    Create problem197_b.java

---
+++ problem197_b.java
@@ -0,0 +1,23 @@
+/* Given an array, rotate the array to the right by k steps, where k is non-negative.
+    An alternative problem to problem 197 - k does not have to be less than the array length so more edge cases
+ */
+public class Main {
+    public void rotate(int[] nums, int k) {
+        if (nums.length == 1){
+            return;
+        }
+        if (k>nums.length){
+            k -= nums.length;
+        }
+        int[] numsCopy = new int[nums.length];
+        for (int i=0; i<nums.length; i++){
+            numsCopy[i] = nums[i];
+        }
+        for (int j=k; j<nums.length; j++){
+            nums[j] = numsCopy[j-k];
+        }
+        for (int i=0; i<k; i++){
+            nums[i] = numsCopy[numsCopy.length-k+i];
+        }
+    }
+}
