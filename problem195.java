58d56eb6d47d7789618d6413323dec252faec656
Jonathan Lu <56990146+jnl27@users.noreply.github.com>
2019-12-30 00:20:47 -0500

    Update problem195.java

--- problem195.java
+++ problem195.java
@@ -1,3 +1,7 @@
+/*Let A be an N by M matrix in which every row and every column is sorted.
+
+Given i1, j1, i2, and j2, compute the number of elements of M smaller than M[i1, j1] and larger than M[i2, j2].
+*/
 public class Main
 {
     public static int computeNumSmallerLarger(int[][] mat, int i1, int i2, int j1, int j2){
